33c0f40ba52956ddc879c23acef6609b5fc762f7
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 14:24:07 +0530

    migrated to postgres

--- pdf_to_text/settings.py
+++ pdf_to_text/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pdf_to_text.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pdf_data',
+        'USER': 'csgeek',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
