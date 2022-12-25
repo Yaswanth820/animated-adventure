e873d84ba396a4471fc78cea52fd10bc5aa4ef73
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 15:21:29 +0530

    implemented list and upload views

--- txt_extract_app/urls.py
+++ txt_extract_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload),
+    path('', views.home, name='list_pdf'),
+    path('<int:pk>/', views.show_pdf, name='show_pdf'),
+    path('upload/', views.upload_pdf, name='upload'),
 ]
--- txt_extract_app/views.py
+++ txt_extract_app/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import uuid
 
+from .models import Document
 from .utils.process_file import handle_uploaded_file
 
 # Create your views here.
-def upload(request):
+def home(request):
+    documents = Document.objects.all()
+    return render(request, 'txt_extract_app/list.html', {'documents': documents})
+
+def show_pdf(request, pk):
+    document = Document.objects.get(id=pk)
+    return render(request, 'txt_extract_app/show_pdf.html', {'document': document})
+
+def upload_pdf(request):
     if request.method == 'POST':
-        file_name = request.FILES['pdf'].name
-        text = handle_uploaded_file(request.FILES['pdf'], file_name)
+        request.FILES['pdf'].name = f'{uuid.uuid4()}-{request.FILES["pdf"].name}'
+        text = handle_uploaded_file(request.FILES['pdf'], request.FILES['pdf'].name)
         print(text)
     return render(request, 'txt_extract_app/upload_pdf.html')
 
