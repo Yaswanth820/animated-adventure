c2b5917b962f7129e0dd88732aed5adb69c9d66d
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 14:17:32 +0530

    implemented file upload

--- pdf_to_text/settings.py
+++ pdf_to_text/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TESSERACT_PATH = r'/opt/homebrew/Cellar/tesseract/5.2.0/bin/tesseract'
--- txt_extract_app/views.py
+++ txt_extract_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .utils.process_file import handle_uploaded_file
+
 # Create your views here.
 def upload(request):
-    return HttpResponse("Hello, world")
+    if request.method == 'POST':
+        file_name = request.FILES['pdf'].name
+        text = handle_uploaded_file(request.FILES['pdf'], file_name)
+        print(text)
+    return render(request, 'txt_extract_app/upload_pdf.html')
+
