4a987246a82772a963cc50c2e5663cd6e64e63da
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 12:52:52 +0530

    setup basic configs

--- pdf_to_text/settings.py
+++ pdf_to_text/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'txt_extract_app.apps.TxtExtractAppConfig',
 ]
 
 MIDDLEWARE = [
