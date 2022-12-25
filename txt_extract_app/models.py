4d4178eb4f04b9597c383845fd748df1f33e474d
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 14:41:07 +0530

    Created document model

--- txt_extract_app/models.py
+++ txt_extract_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    fileName = models.CharField(max_length=255)
+    pdf = models.FileField(upload_to='pdfs/')
+    
+    def __str__(self):
+        return self.fileName
