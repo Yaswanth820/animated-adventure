e77e4b68d12f3a98cdf07cfe471f6886db558e63
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-12-25 16:27:08 +0530

    made small improvements in user auth

--- txt_extract_app/urls.py
+++ txt_extract_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_page, name='register')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
